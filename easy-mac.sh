193bbee73e8a1eb360f1d8c60be691e9e8dfa801
Skye Bleed <skyebleed@mailfence.com>
2019-10-09 17:27:33 -0500

    Add command line option for wireless interface

--- easy-mac.sh
+++ easy-mac.sh
@@ -1,6 +1,9 @@
-# Replace interface with you wireless interface, ex. wlan0
+if [[ -z $1 ]]; then
+	printf "Please enter a wireless interface!\n"
+	exit 1
+fi
 
-ifconfig interface down
-macchanger -r interface
-ifconfig interface up
-exit
+ifconfig $1 down
+macchanger -r $1
+ifconfig $1 up
+exit 0
